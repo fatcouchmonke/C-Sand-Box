cb0e2fd9de4bafd2749362ffe24e802ad3a7540e
fatcouchmonke <70108339+fatcouchmonke@users.noreply.github.com>
2020-08-28 14:00:15 +0200

    updating project
    Pushing information to git to be opened in visual studio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_Sharp_Sand_Box
+{
+    class Program
+    {
+        //this is an attempt in making a calculator
+        static void Main(string[] args) 
+        {
+            int numb01;
+            int numb02;
+
+            Console.Wright ("Type a number to be mulitplied")
+            numb01 = console.Readline();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # C-Sand-Box
 Sand box for learning how to program in C#
+
+Edit done in VS Code
+
+Created the contents using dotnet new console, need to look into dotnet items in powershell
+
